b603c7087c610b7837e2f976a1f3f9bb67dd910a
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-20 21:20:41 +0100

    updates layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,20 +12,18 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className="pt-4">
-        <div className="flex justify-between w-11/12 lg:w-[98%] mx-auto border-b-2 border-grey-50 pb-3">
+        <div className="flex justify-between w-11/12 mx-auto border-b-2 border-grey-50 pb-3">
           <Logo className="w-20" />
           <div className="relative">
-            <Cart className="w-6 sm:w-8" />
+            <Cart className="w-4 sm:w-6" />
 
-            <span className="text-white bg-black text-center absolute py-0.5 px-1 -right-1 -bottom-3 text-[7px] sm:text-[9px]">
+            <span className="text-white bg-black text-center absolute px-1 -right-1 -bottom-2 text-[7px] sm:text-[9px]">
               1
             </span>
           </div>
         </div>
       </header>
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        {children}
-      </main>
+      <main className="w-11/12 mx-auto pt-5">{children}</main>
     </>
   );
 };
