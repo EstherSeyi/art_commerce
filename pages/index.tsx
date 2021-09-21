0a5d781bb8c5cb29b51a3533c193803e33673bfb
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-21 14:36:25 +0100

    updates home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 
 import FeatureItem from "../components/FeatureItem";
@@ -7,10 +8,27 @@ import CaretDown from "../assets/caret-down.svg";
 import Filter from "../assets/filter.svg";
 
 import { i18nCurrencyFormat } from "../helpers/format-curency";
+import useCart from "../custom-hooks/use-cart";
 
 export default function Home() {
+  const { addToCart } = useCart();
+  const [showATC, setShowATC] = useState<{
+    action: boolean;
+    index: null | number;
+  }>({
+    action: false,
+    index: null,
+  });
+  const handleShowingATC = (action: boolean, index: number) => {
+    setShowATC((prevState) => ({
+      ...prevState,
+      action,
+      index,
+    }));
+  };
+
   return (
-    <section className="text-xs sm:text-sm md:text-base">
+    <section>
       <FeatureItem />
       <section className="border-t border-grey-50 mt-10">
         <div className="flex justify-between my-10">
@@ -20,11 +38,14 @@ export default function Home() {
             <span className="text-grey-150">Premium Photos</span>
           </nav>
           <div className="hidden md:flex items-center font-light">
-            <SortIcon className="h-3 w-3 mr-2" />
+            <SortIcon className="h-3 w-3 mr-2 cursor-pointer" />
             <span className="mr-2 text-grey-150">Sort By</span>
             <div className="flex items-center">
-              <span className="mr-1">Price</span>
-              <CaretDown className="h-3 w-3" />
+              <select className="mr-1 focus:outline-none font-thin">
+                <option value="price">Price</option>
+                <option value="a-z">A - Z</option>
+              </select>
+              {/* <CaretDown className="h-3 w-3" /> */}
             </div>
           </div>
 
@@ -51,6 +72,8 @@ export default function Home() {
                   className={`w-full md:max-w-[250px] ${
                     !isLast ? "md:mr-12" : ""
                   }`}
+                  onMouseEnter={() => handleShowingATC(true, index)}
+                  onMouseLeave={() => handleShowingATC(false, index)}
                 >
                   <div className="w-full h-[300px] relative mb-2">
                     <Image
@@ -62,7 +85,14 @@ export default function Home() {
                     />
                     <p
                       aria-roledescription="button"
-                      className="absolute bg-black text-white left-0 right-0 text-center uppercase font-thin bottom-0 py-1 cursor-pointer"
+                      className={`${
+                        showATC.action && showATC.index === index
+                          ? "block"
+                          : "hidden"
+                      } absolute bg-black text-white left-0 right-0 text-center uppercase font-thin bottom-0 py-1 cursor-pointer`}
+                      onClick={() => {
+                        addToCart(item);
+                      }}
                     >
                       Add to cart
                     </p>
@@ -129,30 +159,44 @@ const storeItems = [
     featured: false,
     details: null,
   },
+
   {
-    name: "Man",
-    category: "people",
+    name: "Fly",
+    category: "premium",
     price: 100,
     currency: "USD",
     image: {
-      src: "https://res.cloudinary.com/estherseyi/image/upload/v1632166074/bejamas_assessment/iscktycv8ohw6u1ht4x1.jpg",
-      alt: "man",
+      src: "https://res.cloudinary.com/estherseyi/image/upload/v1632228230/bejamas_assessment/xxqzbyw3z0pyjqbulnsl.jpg",
+      alt: "fly",
     },
     bestseller: false,
     featured: false,
     details: null,
   },
-  // {
-  //   name: "Man",
-  //   category: "people",
-  //   price: 100,
-  //   currency: "USD",
-  //   image: {
-  //     src: "https://res.cloudinary.com/estherseyi/image/upload/v1632166074/bejamas_assessment/iscktycv8ohw6u1ht4x1.jpg",
-  //     alt: "man",
-  //   },
-  //   bestseller: false,
-  //   featured: false,
-  //   details: null,
-  // },
+  {
+    name: "Hands",
+    category: "people",
+    price: 85,
+    currency: "USD",
+    image: {
+      src: "https://res.cloudinary.com/estherseyi/image/upload/v1632228219/bejamas_assessment/bcthiuxljj0v9rq7zi0p.jpg",
+      alt: "hands",
+    },
+    bestseller: false,
+    featured: false,
+    details: null,
+  },
+  {
+    name: "Happy Eggs",
+    category: "food",
+    price: 76,
+    currency: "USD",
+    image: {
+      src: "https://res.cloudinary.com/estherseyi/image/upload/v1632228196/bejamas_assessment/oy6p729syptnj1ki33ca.jpg",
+      alt: "happy eggs",
+    },
+    bestseller: false,
+    featured: false,
+    details: null,
+  },
 ];
