9c313a274bc542f9b2b50e7f928dc22cc3c36898
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-21 14:35:11 +0100

    updates layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,16 @@
-import React, { LegacyRef, useRef, useState } from "react";
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import useClickOutside from "use-click-outside";
+// import useClickOutside from "use-click-outside";
 
 import Logo from "../assets/logo.svg";
 import Cart from "../assets/cart.svg";
 
 import { i18nCurrencyFormat } from "../helpers/format-curency";
+import useCart from "../custom-hooks/use-cart";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const [cartOpen, setCartOpen] = useState(false);
+  const { cart, cartIsOpen, setCartOpen } = useCart();
 
   return (
     <>
@@ -26,10 +27,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             </button>
 
             <span className="text-white bg-black text-center absolute px-1 -right-1 -bottom-0.5 text-[7px] sm:text-[9px]">
-              1
+              {cart?.total ?? 0}
             </span>
           </div>
-          <CartSection setCartOpen={setCartOpen} cartOpen={cartOpen} />
+          <CartSection setCartOpen={setCartOpen} cartOpen={cartIsOpen} />
         </div>
       </header>
       <main className="w-full h-full overflow-hidden">
@@ -50,83 +51,55 @@ const CartSection = ({
   cartOpen: boolean;
   setCartOpen: any;
 }) => {
-  const cartRef = useRef<HTMLDivElement>(null);
-  useClickOutside(cartRef, () => cartOpen && setCartOpen(false));
+  const { cart, clearCart } = useCart();
+  // const cartRef = useRef<HTMLDivElement>(null);
+  // useClickOutside(cartRef, () => cartOpen && setCartOpen(false));
   return (
     <div
       className={`${
         !cartOpen ? "hidden" : "block"
       } absolute bg-white p-6 border border-grey-50 z-[2] right-0 top-7 sm:top-[37px]`}
-      ref={cartRef}
+      // ref={cartRef}
     >
-      <ul className="mb-4 overflow-y-scroll max-h-[80px]">
-        {cartItems.map((item, index) => (
-          <li
-            className="flex pb-2 mb-2 border-b border-grey-50 justify-between"
-            key={`${item.name}_${index}`}
-          >
-            <div className="mr-4">
-              <p>{item.name}</p>
-              <p className="font-light text-grey-100">
-                {i18nCurrencyFormat(item.price)}
-              </p>
-            </div>
-            <div className="w-[50px] h-[50px]  relative">
-              <Image
-                layout="fill"
-                objectFit="cover"
-                alt={item.image.alt}
-                src={item.image.src}
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
-      <button className="w-full border-2 border-black py-2">CLEAR</button>
+      {cart?.items?.length ? (
+        <ul className="mb-4 overflow-y-scroll max-h-[80px]">
+          {cart?.items?.map((item, index) => (
+            <li
+              className="flex pb-2 mb-2 border-b border-grey-50 justify-between"
+              key={`${item.name}_${index}`}
+            >
+              <div className="mr-4">
+                <p>{item.name}</p>
+                <p className="font-light text-grey-100">
+                  {i18nCurrencyFormat(item.price)}
+                </p>
+              </div>
+              <div className="w-[50px] h-[50px]  relative">
+                <Image
+                  layout="fill"
+                  objectFit="cover"
+                  alt={item.image.alt}
+                  src={item.image.src}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-grey-50 text-center py-2">No items yet</p>
+      )}
+      <button
+        className="w-full border-2 border-black py-2"
+        type="button"
+        onClick={() => {
+          clearCart();
+          setCartOpen(false);
+        }}
+      >
+        CLEAR
+      </button>
     </div>
   );
 };
-
-const cartItems = [
-  {
-    name: "Samurai King Resting",
-    category: "people",
-    price: 100,
-    currency: "USD",
-    image: {
-      src: "https://res.cloudinary.com/estherseyi/image/upload/v1632157233/bejamas_assessment/jzy9b3gfumz0caurfoyo.jpg",
-      alt: "dog sitting",
-    },
-    bestseller: false,
-    featured: false,
-    details: null,
-  },
-  {
-    name: "Samurai King Resting",
-    category: "people",
-    price: 100,
-    currency: "USD",
-    image: {
-      src: "https://res.cloudinary.com/estherseyi/image/upload/v1632157233/bejamas_assessment/jzy9b3gfumz0caurfoyo.jpg",
-      alt: "dog sitting",
-    },
-    bestseller: false,
-    featured: false,
-    details: null,
-  },
-  {
-    name: "Samurai King Resting",
-    category: "people",
-    price: 100,
-    currency: "USD",
-    image: {
-      src: "https://res.cloudinary.com/estherseyi/image/upload/v1632157233/bejamas_assessment/jzy9b3gfumz0caurfoyo.jpg",
-      alt: "dog sitting",
-    },
-    bestseller: false,
-    featured: false,
-    details: null,
-  },
-];
 
 export default Layout;
