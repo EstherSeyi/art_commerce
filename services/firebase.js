4512c2457dfe94f1d8053f9645a0d3d2631a351c
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-22 21:47:50 +0100

    updates firebase service

--- services/firebase.js
+++ services/firebase.js
@@ -1,5 +1,4 @@
 import {
-  getFirestore,
   collection,
   getDocs,
   getDoc,
@@ -8,6 +7,8 @@ import {
   orderBy,
   limit,
   startAfter,
+  endBefore,
+  limitToLast,
 } from "firebase/firestore/lite";
 import { db } from "../config/firebase";
 
@@ -18,49 +19,37 @@ export async function getImageById(imageId) {
   return result;
 }
 
-const initialProducts = (last, by, direction) => {
-  return query(
-    collection(db, "products"),
-    limit(6),
-    orderBy(by, direction),
-    startAfter(last ?? "")
-  );
+const initialProducts = (last, by, direction, first, type) => {
+  if (type === "prev") {
+    return query(
+      collection(db, "products"),
+      orderBy(by, direction),
+      endBefore(first),
+      limitToLast(6)
+    );
+  } else {
+    return query(
+      collection(db, "products"),
+      limit(6),
+      orderBy(by, direction),
+      startAfter(last ?? "")
+    );
+  }
 };
 
 // Get a list of products from database
-export async function getProducts(last, by, direction) {
-  const prodductsCol = initialProducts(last, by ?? "name", direction ?? "asc");
+export async function getProducts(last, by, direction, first, type) {
+  const prodductsCol = initialProducts(
+    last ?? "",
+    by ?? "name",
+    direction ?? "asc",
+    first,
+    type
+  );
 
   const productSnapshot = await getDocs(prodductsCol);
   const productList = productSnapshot.docs.map((doc) => {
     return { ...doc.data(), docId: doc.id };
   });
   return productList;
-}
-
-export async function sortProducts(by) {
-  const q = query(collection(db, "products"), orderBy(by, "desc"), limit(6));
-  const x = await getDocs(q);
-
-  return x.docs.map((doc) => {
-    return { ...doc.data(), docId: doc.id };
-  });
-}
-
-export async function getPaginated(last = "", by, direction, type = "next") {
-  const next = query(
-    collection(db, "products"),
-    orderBy(by ?? "name", direction ?? "asc"),
-    startAfter(last),
-    limit(6)
-  );
-  const nextPage = await getDocs(next);
-
-  if (type === "next") {
-    return nextPage.docs.map((doc) => {
-      return { ...doc.data(), docId: doc.id };
-    });
-  } else {
-    return [];
-  }
 }
